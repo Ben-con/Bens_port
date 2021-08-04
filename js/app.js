9289d10c0bcfb6a28c56d7aa5df954e42d794065
Ben.con <041ymoob@gmail.com>
2021-08-04 13:17:09 -0700

    starting buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+
+const selectAll = document.getElementsByTagName("*");
+const htmlDemo = document.getElementsByClassName("html-demo")
+
+htmlDemo.addEventListener("click",e =>{
+    if(e.target.classList.contains("not-red")){
+        htmlDemo.classList.remove("not-red")
+       selectAll.style.border = "1px red solid"
+    }else{
+        htmlDemo.classList.add("not-red") 
+        selectAll.style.border = "none"
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const projectsUnhide = document.querySelector(".un-hide");
 const projectHide = document.querySelector(".hide")
 
