2566694b55b4e8d1814d33a1c3d49ad20c6ebf5e
Ben.con <041ymoob@gmail.com>
2021-08-05 14:35:30 -0700

    buttons done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,86 @@
+const javascriptExample1 = document.querySelector("#javascript-demo-ex1")
+const javascriptExample2 = document.querySelector("#javascript-demo-ex2")
+const javascriptExample3 = document.querySelector("#javascript-demo-ex3")
+const javascriptExample4 = document.querySelector(".javascript-demo-ex4")
+const javascriptExample5 = document.querySelector("#javascript-demo-ex5")
+const javascriptExample6 = document.querySelector("#javascript-demo-ex6")
+const javascriptExample7 = document.querySelector("#javascript-demo-ex7")
+const javascriptExample8 = document.querySelector("#javascript-demo-ex8")
+const javascriptDemoPar = document.querySelector("#javascript-demo-p")
+const javascriptDemoTitle = document.querySelector("#javascript-demo-title")
+const javascriptDemo = document.querySelector("#javascript-demo")
 
+javascriptDemo.addEventListener("click",e => {
+    if(e.target.classList.contains("javascript-demo-changeme")){
+        javascriptDemoPar.textContent = "JavaScript is amazing you can you can change almost everying on a webpage with it. Just look at the navigation bar"
+        setTimeout(() => { javascriptExample1.textContent = "Like me",javascriptExample1.classList.add("javascript-demo-class"); }, 4000); 
+        setTimeout(() => {  javascriptExample2.textContent = "And me",javascriptExample2.classList.add("javascript-demo-class"); }, 5000); 
+        setTimeout(() => {  javascriptExample3.textContent = "Also me",javascriptExample3.classList.add("javascript-demo-class"); },6000); 
+        setTimeout(() => {  javascriptExample4.textContent = "Click me",javascriptExample4.classList.add("javascript-demo-class"); }, 7000); 
+        setTimeout(() => {  javascriptExample5.textContent = "Even in here",javascriptExample5.classList.add("javascript-demo-class"); }, 9000); 
+        setTimeout(() => {  javascriptExample6.textContent = "you can change",javascriptExample6.classList.add("javascript-demo-class"); }, 10000);  
+        setTimeout(() => {  javascriptExample7.textContent = "everything",javascriptExample7.classList.add("javascript-demo-class"); }, 11000);  
+        setTimeout(() => {   javascriptExample8.textContent = "Click the button again",javascriptExample8.classList.add("javascript-demo-class"); }, 12000); 
+        setTimeout(() => {  javascriptDemo.textContent = "Click me again!"; }, 13000); 
+        javascriptDemo.classList.remove("javascript-demo-changeme")
+        javascriptDemo.classList.add("javascript-demo-changeme2")
+    }else if(e.target.textContent === "Click me again!"){
+        javascriptExample1.textContent = "Home",javascriptExample1.classList.remove("javascript-demo-class")
+        javascriptExample2.textContent = "About me",javascriptExample2.classList.remove("javascript-demo-class")
+        javascriptExample3.textContent = "Contact me",javascriptExample3.classList.remove("javascript-demo-class")
+        javascriptExample4.textContent = "Projects",javascriptExample4.classList.remove("javascript-demo-class")
+        javascriptExample5.textContent = "Web App",javascriptExample5.classList.remove("javascript-demo-class")
+        javascriptExample6.textContent = "online Directory",javascriptExample6.classList.remove("javascript-demo-class")
+        javascriptExample7.textContent = "Web Form",javascriptExample7.classList.remove("javascript-demo-class")
+        javascriptExample8.textContent = "Photo Gallery",javascriptExample8.classList.remove("javascript-demo-class")
+        javascriptDemoPar.textContent = "As you can see java script is a very powerful tool. Click the button one more time to switch everything back to normal!"
+        javascriptDemo.textContent ="Click me one more time"
+        javascriptDemo.classList.remove("javascript-demo-changeme2")
+    }else{
+        javascriptDemo.classList.add("javascript-demo-changeme")
+        javascriptDemoPar.textContent = "Click below to see an amazing example of the wonderful tool JavaScript"
+        javascriptDemo.textContent = "Click me!"
+    }
+})
+
+
+
+
+
+
+
+// -----------------------Css Demo start------------------------------------------
+const cssDemo = document.querySelector(".css-demo")
+const cssDemoPar = document.querySelector(".css-demo-p")
+const cssDemoTitle = document.querySelector(".css-demo-title")
+const cssDemoBody = document.querySelector(".css-demo-body")
+
+cssDemo.addEventListener("click", e =>{
+    if(cssDemo.id === "css-demo-start"){
+        cssDemoPar.textContent = "CSS is a great tool that lets me change the look of everything that you are looking at on this webpage!"
+        cssDemoPar.style.color = "red"
+        cssDemoTitle.style.color = "red"
+        cssDemoBody.style.borderRadius = "100%"
+        cssDemoBody.style.backgroundColor = "#313639"
+        cssDemo.textContent = "Click me again!"
+        cssDemo.id = "css-demo-end"
+    }else{
+        cssDemoTitle.style.color = "black"
+        cssDemoPar.style.color = "black"
+        cssDemoBody.style.backgroundColor = "white"
+        cssDemoBody.style.borderRadius = "3em"
+        cssDemo.textContent = "Click me!"
+        cssDemoPar.textContent = "Without CSS everything on this webpage would be all out of center as well as being very boring!"
+        cssDemo.id = "css-demo-start"
+    }
+})
+// -----------------------Css Demo end------------------------------------------
+
+// -----------------------HTML Demo start------------------------------------------
 const htmlDemo = document.querySelector(".html-demo")
 const htmlDemoPar = document.querySelector(".html-demo-p")
 const selectAll = document.getElementsByTagName("*");
 
-// help here please.
 htmlDemo.addEventListener("click",e =>{
     if(e.target.classList.contains("not-red")){
         for (let i = 0; i < selectAll.length; i++) {
@@ -21,20 +98,9 @@ htmlDemo.addEventListener("click",e =>{
         
     }
 })
+// -----------------------HTML Demo end------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// -----------------------See more projects start------------------------------------------
 const projectsUnhide = document.querySelector(".un-hide");
 const projectHide = document.querySelector(".hide")
 
@@ -51,5 +117,6 @@ projectsUnhide.addEventListener("click",e =>{
         projectsUnhide.textContent = "Click to view more Projects";
     }
 })
+// -----------------------See more projects end------------------------------------------
 
 
